addc60c055fc6133585b037372b5ede6b69687b1
leaveys <leaveys@gmail.com>
2014-08-03 16:41:31 -0500

    Added comments
    Added descriptive comments.

--- webserver.rb
+++ webserver.rb
@@ -1,9 +1,12 @@
+#Install the Httpd Apache Web Server
 package 'httpd'
 
+#Ensure the Httpd web service is started and enabled
 service 'httpd' do
 	action [:start, enable]
 end
 
+#Create a static HTML file in /var/www/html/
 file '/var/www/html/index.html' do
 	content '<html>
 		<title>Hello Stelligent!</title>
